622daf4dfe21809b54b70e9786bf262c296f8fd3
VAYR_hugo <77483277+sartek430@users.noreply.github.com>
2022-01-05 12:06:38 +0100

    création des tableaux de questions

--- app/src/main/java/com/example/quizz/MainActivity.java
+++ app/src/main/java/com/example/quizz/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.quizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+
+        Context context = getApplicationContext();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizz/QuestionActivity.java
+++ app/src/main/java/com/example/quizz/QuestionActivity.java
@@ -3,12 +3,54 @@ package com.example.quizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btn7;
+    String[] questions;
+    String[] reponses;
+    String[] badReponses;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question);
+
+        btn4 = findViewById(R.id.button11);
+        btn5 = findViewById(R.id.button12);
+        btn6 = findViewById(R.id.button13);
+        btn7 = findViewById(R.id.button14);
+
+        questions = new String[4];
+        questions[0] = "En quelle année s'est déroulé la bataille de Marignan ?";
+        questions[1] = "La place de la femme c'est où ?";
+        questions[2] = "Quel est le meilleur couple de tous les temps ?";
+        questions[3] = "Qui a succéder napoleon 3 en 1871 ?";
+
+        reponses = new String[4];
+        reponses[0] = "1515";
+        reponses[1] = "La cuisine";
+        reponses[2] = "Elsa et Michou";
+        reponses[3] = "Adolphe Thiers";
+
+        badReponses = new String[12];
+        badReponses[0] = "ça n'a jamais eu lieu";
+        badReponses[1] = "2022";
+        badReponses[2] = "13";
+        badReponses[3] = "La chambre";
+        badReponses[4] = "La cave";
+        badReponses[5] = "La cuisine";
+        badReponses[6] = "Chèvre et Miel";
+        badReponses[7] = "Hugo et Geoffrey";
+        badReponses[8] = "Cécile et Fred";
+        badReponses[9] = "Eric Zemmour";
+        badReponses[10] = "Hugo";
+        badReponses[11] = "Napoleon 4";
+
     }
 }
